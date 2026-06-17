59419a67591a6898745aa707563066c470eead26
KanaeSUMI <net.ma.l.sumika@gmail.com>
2026-06-18 01:03:23 +0900

    数値の修正、continueを覚えた

--- cipher_2.py
+++ cipher_2.py
@@ -1,5 +1,5 @@
 text = input("文字列を入力してください：")
-amount = int(input("ずらす文字数を入力してください(0~25)："))
+amount = int(input("ずらす文字数を入力してください："))
 
 result = ""
 
@@ -8,14 +8,13 @@ for letter in text:
     number = ord(letter)
     range_size  = 26
     
-    if 123 >= number >= 97:
+    if 122 >= number >= 97:
         base = 97
-    elif 91 >= number >=65:
+    elif 90 >= number >=65:
         base = 65
     else:
-        base = 0
-        amount = 0
-        range_size = number + 1
+        result = result + letter
+        continue
 
     number = number - base
     number = (number + amount) % range_size
